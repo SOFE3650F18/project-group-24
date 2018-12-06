262a66663e0e4b986c222ed8fb9570df359bd195
WalidSafi <32139490+WalidSafi@users.noreply.github.com>
2018-12-05 23:34:36 -0500

    Create AssessmentDonebyWalid Safi.d

---
+++ Team assement/AssessmentDonebyWalid Safi.d
@@ -0,0 +1,7 @@
+| Criteria | Score |
+|-----------------------------------|--------------------------|
+| Commitment | 5 |
+| Communication | 5 |
+| Knowledge, Skills, Abilities, etc | 5 |
+| Ethical Standards | 5 |
+| Focus | 5 |
