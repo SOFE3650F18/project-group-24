4072a12cba2366135f1969d727b5c4c0e33ba66c
WalidSafi <32139490+WalidSafi@users.noreply.github.com>
2018-12-05 23:35:36 -0500

    Update AssessmentDonebyWalid Safi.d

--- Team assement/AssessmentDonebyWalid Safi.d
+++ Team assement/AssessmentDonebyWalid Safi.d
@@ -1,5 +1,5 @@
 | Criteria | Score |
-|-----------------------------------|--------------------------|
+|---|---|
 | Commitment | 5 |
 | Communication | 5 |
 | Knowledge, Skills, Abilities, etc | 5 |
